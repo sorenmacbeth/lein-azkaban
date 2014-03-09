574a257a6d6f05dbe633656a41325f03290bb692
Soren Macbeth <soren@dopeness.org>
2014-03-09 14:32:55 -0700

    add execute (doesn't seem to be working though)

--- src/lein_azkaban/core.clj
+++ src/lein_azkaban/core.clj
@@ -30,12 +30,31 @@
     (connect! (:endpoint config) (:username config) (:password config))
     (with-endpoint (str (:endpoint config) "/manager")
       (let [response (c/post *endpoint*
-               {:multipart [{:name "session.id" :content *session-id*}
-                            {:name "ajax" :content "upload"}
-                            {:name "project" :content (:project config)}
-                            {:name "Content/type" :content "application/zip"}
-                            {:name "file" :content (clojure.java.io/file path)}]})]
+                             {:multipart [{:name "session.id" :content *session-id*}
+                                          {:name "ajax" :content "upload"}
+                                          {:name "project" :content (:project config)}
+                                          {:name "Content/type" :content "application/zip"}
+                                          {:name "file" :content (clojure.java.io/file path)}]})]
         (if (= (:status response) 200)
           (let [body (:body response)
                 json (json/parse-string body true)]
-            (println (format "successfully uploaded %s project. new version: %s" (:project config) (:version json)))))))))
+            (println (format "successfully uploaded %s project. new version: %s" (:project config) (:version json))))
+          (println (format "unable to connect to endpoint. got http code: %s" (:status response))))))))
+
+(defn execute
+  "Execute a flow named `flow`."
+  [project [flow]]
+  (let [config (:azkaban project)]
+    (connect! (:endpoint config) (:username config) (:password config))
+    (with-endpoint (str (:endpoint config) "/manager")
+      (let [response (c/post *endpoint* {:form-params {:ajax "executeFlow"
+                                                       :session.id *session-id*
+                                                       :project (:project config)
+                                                       :flow flow}})]
+        (if (= (:status response) 200)
+          (let [body (:body response)
+                json (json/parse-string body true)]
+            (if-let [error (:error json)]
+              (println (format "unable to execute flow %s: %s") flow error)
+              (println json)))
+          (println (format "unable to connect to endpoint. got http code: %s" (:status response))))))))
--- src/leiningen/azkaban.clj
+++ src/leiningen/azkaban.clj
@@ -1,10 +1,11 @@
 (ns leiningen.azkaban
-  (:require [lein-azkaban.core :refer [upload]]))
+  (:require [lein-azkaban.core :refer [upload execute]]))
 
 (defn azkaban
   "Interact with Azkaban"
-  {:help-arglist '([upload])
-   :subtasks [#'upload]}
+  {:help-arglist '([upload execute])
+   :subtasks [#'upload #'execute]}
   [project subtask & args]
   (case subtask
-    "upload" (upload project args)))
+    "upload" (upload project args)
+    "execute" (execute project args)))
