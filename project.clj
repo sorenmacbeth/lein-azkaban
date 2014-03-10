3853019ece3c5f8fa349e2b0a54c1056625e229d
Soren Macbeth <soren@dopeness.org>
2014-03-09 20:50:26 -0700

    bump snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-azkaban "0.1.0"
+(defproject lein-azkaban "0.1.1-SNAPSHOT"
   :description "A Leiningen plugin for interacting with Azkaban"
   :url "https://github.com/sorenmacbeth/lein-azkaban"
   :license {:name "Eclipse Public License"
